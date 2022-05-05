568a9afe4a8ab984ee94f7dbcc1cff91f0f69fae
horaciochacon <horaciochacon89@gmail.com>
2022-05-05 08:00:45 +0000

    final MRBRT model ready, cubic, maxder 0.05

--- src/01_Format_data.R
+++ src/01_Format_data.R
@@ -1,70 +1,29 @@
+# Load Packages -----------------------------------------------------------
+
 library(dplyr)
-library(janitor)
 library(lubridate)
-library(stringr)
 library(ggplot2)
 library(reticulate)
 use_condaenv("mrtool-0.0.1")
 library(mrtoolr)
 library(ggpubr)
-library(readxl)
-library(stringi)
-library(stringr)
+library(cowplot)
 
 # Loading and Formatting the data -----------------------------------------
 
-
 # Death count per day dataset
-death_count_day <- read.csv("data/death_count_prov_day.csv",
+death_count_day <- read.csv(
+  "data/death_count_prov_day.csv",
   colClasses = c(
     "Date","character", "character","character","character","numeric"
-    )
-  ) %>% 
+  )
+) %>% 
   tibble()
 
 # Province level population
 poblacion_prov <- read.csv("data/poblacion_provincial_peru.csv") %>% 
   group_by(dpt_cdc = DEPARTAMENTO,prov_cdc = PROVINCIA) %>% 
   summarise(pob = sum(POBLACION))
-
-# Department level population
-poblacion_dpt <- read.csv("data/poblacion_provincial_peru.csv") %>% 
-  group_by(dpt_cdc = DEPARTAMENTO) %>% 
-  summarize(pob = sum(POBLACION))
-
-# National weekly death count 
-death_count_ntl <- death_count_day %>% 
-  mutate(
-    fecha_fallecimiento = floor_date(
-      fecha_fallecimiento, "weeks", week_start = 1
-    )
-  ) %>% 
-  group_by(fecha_fallecimiento) %>% 
-  summarise(n = sum(n)) %>% 
-  mutate(
-    week = (as.numeric(fecha_fallecimiento) - 18323) / 7
-  )
-
-# Department level weekly death counts
-data_dpt <- death_count_day %>% 
-  mutate(
-    fecha_fallecimiento = floor_date(
-      fecha_fallecimiento, "weeks", week_start = 1
-    )
-  ) %>% 
-  group_by(fecha_fallecimiento, dpt_cdc) %>% 
-  summarise(n = sum(n)) %>% 
-  left_join(poblacion_dpt, by = "dpt_cdc") %>% 
-  ungroup() %>% 
-  mutate(
-    y1 = n / pob,
-    ylog = log(y1 + 0.0000001),
-    x1 = (as.numeric(fecha_fallecimiento) - 18323) / 7,
-    sd = sqrt(1 / y1),
-    id = row_number()
-  ) %>% 
-  na.omit() 
-
 
 # Offset of 0.00001
 # Province level death counts
@@ -74,46 +33,69 @@ data_prov <- death_count_day %>%
       fecha_fallecimiento, "weeks", week_start = 1
     )
   ) %>% 
-  group_by(fecha_fallecimiento, dpt_cdc, prov_cdc) %>% 
+  group_by(fecha_fallecimiento, dpt_cdc, prov_cdc) %>%
+  # filter(dpt_cdc == "ANCASH") %>%
   summarise(n = sum(n)) %>% 
   left_join(poblacion_prov, by = "prov_cdc") %>% 
   ungroup() %>% 
   mutate(
     y1 = n / pob,
-    ylog = log(y1 + 0.0000001),
+    ylog = log(y1 + min(y1, na.rm = TRUE)/2),
     x1 = (as.numeric(fecha_fallecimiento) - 18323) / 7,
-    sd = sqrt(1 / (y1 + 0.0000001))
+    # sd = sqrt(
+    #   (y1 + min(y1, na.rm = TRUE)/2) *(1 - (y1 + min(y1, na.rm = TRUE)/2)) / pob
+    #   ),
+    # sd = log(sqrt(1 / (y1 + min(y1)/2)))
+    # sd = 1,
+    sd = sqrt(
+      (1 - (y1 + min(y1, na.rm = TRUE)/2)) / 
+        ((y1 + min(y1, na.rm = TRUE)/2) * pob)
+    ),
+    sd2 = sqrt((1) / (y1 + min(y1, na.rm = TRUE)/2)),
+    y_low = ylog - (1.96 * sd),
+    y_upp = ylog + (1.96 * sd)
   ) %>% 
   na.omit() %>% 
   select(-dpt_cdc.y, dpt_cdc = dpt_cdc.x, -fecha_fallecimiento)
-  
+
 # Baseline Province level data y1 = 0
-province_baseline <- map(
+province_baseline <- purrr:::map(
   .x = list(-3,-2,-1),
-  .f = ~ data_prov %>% 
-    group_by(dpt_cdc, prov_cdc, pob) %>% 
-    summarize(x1 = min(x1)) %>% 
+  .f = ~ data_prov %>%
+    group_by(dpt_cdc, prov_cdc, pob) %>%
+    summarize(x1 = min(x1)) %>%
     mutate(
-      x1 = x1 + .x,
+      # x1 = x1 + .x,
+      x1 = 0 + .x,
       y1 = 0,
-      ylog = log(0 + 0.0000001),
+      ylog = log(0 + min(data_prov$y1)/2),
       n = 0,
-      sd = sqrt(1 / (y1 +  0.0000001)),
+      # sd = 
+      #   sqrt(
+      #     (y1 + min(data_prov$y1)/2) * (1 - (y1 + min(data_prov$y1)/2)) / pob
+      #     )
+      
+      sd = 1,
+      # sd = sqrt(
+      #   (1 - (y1 + min(data_prov$y1)/2)) /
+      #     ((y1 + min(data_prov$y1)/2) * pob)
+      #   ),
+      y_low = ylog - (1.96 * sd),
+      y_upp = ylog + (1.96 * sd)
     )
-  ) %>% 
+) %>%
   bind_rows()
 
 # Final provincial dataset with baseline dummy data
-data_prov <- data_prov %>% 
-  bind_rows(province_baseline) %>% 
-  arrange(dpt_cdc, prov_cdc, pob, x1) %>% 
+data_prov <- data_prov %>%
+  bind_rows(province_baseline) %>%
+  arrange(dpt_cdc, prov_cdc, pob, x1) %>%
   mutate(
     id = row_number(),
     x1 = x1 +3
   )
 
-
-# Splines Prov ---------------------------------------------------------------
+# MR-BRT Model ---------------------------------------------------------------
 
 prov_mrbrt <- MRData()
 prov_mrbrt$load_df(
@@ -122,7 +104,7 @@ prov_mrbrt$load_df(
   col_obs_se = "sd",
   col_covs = list("x1"), 
   col_study_id = "id" 
-  )
+)
 
 
 mod_prov <- MRBRT(
@@ -133,16 +115,16 @@ mod_prov <- MRBRT(
       alt_cov = "x1",
       use_spline = TRUE,
       # spline_knots = array(c(0, 0.25, 0.5, 0.75, 1)),
-      spline_knots = array(c(seq(0, 1, by = 0.08), 1)),
-      spline_degree = 3L,
+      spline_knots = array(c(seq(0, 1, by = 0.1))),
+      spline_degree = 2L,
       spline_knots_type = 'frequency',
       # spline_r_linear = FALSE,
-      spline_l_linear = TRUE,
-      # prior_spline_convexity_domain = array(c(0, 0.03)),
-      prior_spline_monotonicity = 'increasing',
-      prior_spline_monotonicity_domain = array(c(0, 0.05))
-      # prior_spline_convexity = "convex"
-      # prior_spline_maxder_gaussian = array(c(0, 0.01))
+      # spline_l_linear = TRUE,
+      # prior_spline_convexity = "convex",
+      # prior_spline_convexity_domain = array(c(0, 0.03))
+      # prior_spline_monotonicity = 'increasing',
+      # prior_spline_monotonicity_domain = array(c(0, 0.01))
+      prior_spline_maxder_gaussian = array(c(0, 0.05))
     )
   )
 )
@@ -159,17 +141,15 @@ dat_pred_prov$load_df(
 
 pred_prov <- mod_prov$predict(data = dat_pred_prov)
 
-ggplot(data_prov) +
-  geom_point(aes(x = x1, y = y1 * 100000), alpha = 0.3, size = 0.3) +
-  geom_line(data = df_pred, aes(x = x1, y = exp(pred_prov) * 100000)) +
-  ylim(c(0, 30)) +
-  labs(x = "Week", y = "Mortality per 100,000") +
-  theme_bw()
- 
+# ggplot(data_prov) +
+#   geom_point(aes(x = x1, y = y1 * 100000), alpha = 0.3, size = 0.3) +
+#   geom_line(data = df_pred, aes(x = x1, y = exp(pred_prov) * 100000)) +
+#   labs(x = "Week", y = "Mortality per 100,000") +
+#   theme_bw()
 
-# Cascade splines Dep with Prov data -----------------------------------------
+# Cascade splines Dep  ----------------------------------------------------
 
-mod_spline_dpto2 <- run_spline_cascade(
+mod_spline_dpto <- run_spline_cascade(
   stage1_model_object = mod_prov,
   df = data_prov,
   col_obs = "ylog",
@@ -184,24 +164,24 @@ mod_spline_dpto2 <- run_spline_cascade(
 
 df_pred <- expand.grid(
   stringsAsFactors = FALSE,
-  x1 = seq(0, 80, by = 0.1),
-  dpt_cdc = unique(data_dpt$dpt_cdc)
-  ) %>%
+  x1 = seq(0, 85, by = 0.1),
+  dpt_cdc = unique(data_prov$dpt_cdc)
+) %>%
   mutate(data_id = 1:nrow(.)) 
 
-pred_cascade_dpto2 <- predict_spline_cascade(
-  fit = mod_spline_dpto2,
+pred_cascade_dpto <- predict_spline_cascade(
+  fit = mod_spline_dpto,
   newdata = df_pred
-  ) %>% 
+) %>% 
   left_join(data_prov)
 
-ggplot(data = pred_cascade_dpto2) +
-  geom_line(aes(x = x1, y = exp(pred) * 100000)) +
-  geom_point(aes(x = x1, y = y1 * 100000), size = 0.1, alpha = 0.5) +
-  facet_wrap(.~dpt_cdc) +
-  labs(x = "Week", y = "Mortality per 100,000") +
-  ylim(c(0,60)) +
-  theme_bw()
+# ggplot(data = pred_cascade_dpto) +
+#   geom_line(aes(x = x1, y = exp(pred) * 100000)) +
+#   geom_point(aes(x = x1, y = y1 * 100000), size = 0.1, alpha = 0.5) +
+#   facet_wrap(.~dpt_cdc) +
+#   ylim(c(0,50)) +
+#   labs(x = "Week", y = "Mortality per 100,000") +
+#   theme_bw()
 
 # Cascade splines Prov --------------------------------------------------------
 
@@ -212,7 +192,7 @@ mod_spline_prov <- run_spline_cascade(
   col_obs_se = "sd",
   col_study_id = "id",
   stage_id_vars = c("dpt_cdc", "prov_cdc"),
-  thetas = c(5,10),
+  thetas = c(10, 10),
   output_dir = "output/",
   model_label = "mbrt_cascade_peru_prov",
   overwrite_previous = TRUE
@@ -223,58 +203,130 @@ df_pred <- expand.grid(
   stringsAsFactors = FALSE,
   x1 = seq(0, 80, by = 0.1),
   prov_cdc = unique(data_prov$prov_cdc)
-  ) %>%
-  mutate(
-    data_id = 1:nrow(.)
-  ) %>% 
+) %>%
+  mutate(data_id = 1:nrow(.)) %>% 
   left_join(poblacion_prov %>% select(-pob))
 
 preds_cascade_prov <- predict_spline_cascade(
   fit = mod_spline_prov,
   newdata = df_pred
-  ) %>% 
+) %>% 
   left_join(data_prov) 
 
-depts <- unique(data_dpt$dpt_cdc)
+depts <- unique(data_prov$dpt_cdc)
+# depts <- "LORETO"
 
 for (i in depts) {
   
-  graph_dpto <- pred_cascade_dpto2 %>% 
-    filter(dpt_cdc == i) %>% 
+  graph_dpto <- pred_cascade_dpto %>%
+    filter(dpt_cdc == i) %>%
     ggplot() +
-    geom_line(aes(x = x1, y = exp(pred) * 100000)) +
+    geom_errorbar(
+      aes(
+        x = x1,
+        y = y1 * 1e5,
+        ymin = exp(y_low) * 1e5,
+        ymax = exp(y_upp) * 1e5
+      ),
+      size = 0.2) +
     geom_point(
-      data = data_prov  %>% filter(dpt_cdc == i), 
-      aes(x = x1, y = y1 * 100000), 
-      size = 1, 
-      alpha = 0.5
-      ) +
+      data = data_prov  %>% filter(dpt_cdc == i),
+      aes(x = x1, y = y1 * 100000),
+      size = 0.7,
+      alpha = 0.4
+    ) +
+    geom_line(aes(x = x1, y = exp(pred) * 100000), col = "red") +
     facet_wrap(.~dpt_cdc) +
+    labs(x = NULL, y = "Mortality per 100,000") +
+    theme_bw()
+  
+  graph_prov <- preds_cascade_prov %>%
+    filter(dpt_cdc == i) %>%
+    ggplot() +
+    geom_point(aes(x = x1, y = y1 * 100000), size = 1, alpha = 0.5) +
+    geom_errorbar(
+      aes(
+        x = x1,
+        y = y1 * 1e5,
+        ymin = exp(y_low) * 1e5,
+        ymax = exp(y_upp) * 1e5
+      ),
+      size = 0.2) +
+    geom_line(aes(x = x1, y = exp(pred) * 100000), col = "blue") +
+    facet_wrap(.~prov_cdc) +
+    coord_cartesian(
+      ylim = c(
+        0,
+        max(
+          preds_cascade_prov %>% filter(dpt_cdc == i) %>% .$y1,
+          na.rm = TRUE
+        ) * 1e5 + 5
+      )
+    )+
     labs(x = "Week", y = "Mortality per 100,000") +
     theme_bw()
   
-  graph_prov <- preds_cascade_prov %>% 
-    filter(dpt_cdc == i) %>% 
+  graph <-  ggdraw() +
+    draw_plot(graph_dpto, x = 0.25, y = .7, width = .5, height = .3) +
+    draw_plot(graph_prov, x = 0, y = 0, width = 1, height = 0.7)
+  
+  graph_dpto_log <- pred_cascade_dpto %>%
+    filter(dpt_cdc == i) %>%
     ggplot() +
-    geom_line(aes(x = x1, y = exp(pred) * 100000)) +
-    geom_point(aes(x = x1, y = y1 * 100000), size = 1, alpha = 0.5) +
+    geom_errorbar(
+      aes(
+        x = x1,
+        y = ylog,
+        ymin = y_low,
+        ymax = y_upp
+      ),
+      size = 0.2) +
+    geom_point(
+      data = data_prov  %>% filter(dpt_cdc == i),
+      aes(x = x1, y = ylog),
+      size = 0.7,
+      alpha = 0.4
+    ) +
+    geom_line(aes(x = x1, y = pred), col = "red") +
+    facet_wrap(.~dpt_cdc) +
+    labs(x = NULL, y = "Mortality per 100,000") +
+    theme_bw()
+
+  graph_prov_log <- preds_cascade_prov %>%
+    filter(dpt_cdc == i) %>%
+    ggplot() +
+    geom_point(aes(x = x1, y = ylog), size = 1, alpha = 0.5) +
+    geom_errorbar(
+      aes(
+        x = x1,
+        y = ylog,
+        ymin = y_low,
+        ymax = y_upp
+      ),
+      size = 0.2) +
+    geom_line(aes(x = x1, y = pred), col = "blue") +
     facet_wrap(.~prov_cdc) +
     labs(x = "Week", y = "Mortality per 100,000") +
-    theme_bw() 
-  
-  graph <- ggarrange(
-    graph_dpto,
-    graph_prov, 
-    nrow = 2,
-    heights = c(2, 5), 
-    widths = c(0.5,5)
-    )
-  
+    theme_bw()
+
+  graph_log <-  ggdraw() +
+    draw_plot(graph_dpto_log, x = 0.25, y = .7, width = .5, height = .3) +
+    draw_plot(graph_prov_log, x = 0, y = 0, width = 1, height = 0.7)
+
+  print(graph_log)
   print(graph)
   
-  ggsave(plot = graph, filename =  paste0("plots/",i, ".png"), scale = 2)
+  ggsave(
+    plot = graph_log, 
+    filename =  paste0("plots/final/log-",i, ".png"), 
+    scale = 2
+    )
+  ggsave(
+    plot = graph,
+    filename =  paste0("plots/final/exp-",i, ".png"),
+    scale = 2
+    )
 }
-
 
 prov_time_series <- predict_spline_cascade(
   fit = mod_spline_prov,
--- src/05_MinRepExample.R
+++ src/05_MinRepExample.R
@@ -34,17 +34,26 @@ data_prov <- death_count_day %>%
     )
   ) %>% 
   group_by(fecha_fallecimiento, dpt_cdc, prov_cdc) %>%
-  filter(dpt_cdc == "ANCASH") %>%
+  # filter(dpt_cdc == "ANCASH") %>%
   summarise(n = sum(n)) %>% 
   left_join(poblacion_prov, by = "prov_cdc") %>% 
   ungroup() %>% 
   mutate(
     y1 = n / pob,
-    ylog = log(y1 + min(y1)/2),
+    ylog = log(y1 + min(y1, na.rm = TRUE)/2),
     x1 = (as.numeric(fecha_fallecimiento) - 18323) / 7,
-    sd = log(sqrt(1 / (y1 + min(y1)/2)))
-    # sd = 1
-    # sd = log(sqrt(1 / y1))
+    # sd = sqrt(
+    #   (y1 + min(y1, na.rm = TRUE)/2) *(1 - (y1 + min(y1, na.rm = TRUE)/2)) / pob
+    #   ),
+    # sd = log(sqrt(1 / (y1 + min(y1)/2)))
+    # sd = 1,
+    sd = sqrt(
+      (1 - (y1 + min(y1, na.rm = TRUE)/2)) / 
+        ((y1 + min(y1, na.rm = TRUE)/2) * pob)
+      ),
+    sd2 = sqrt((1) / (y1 + min(y1, na.rm = TRUE)/2)),
+    y_low = ylog - (1.96 * sd),
+    y_upp = ylog + (1.96 * sd)
   ) %>% 
   na.omit() %>% 
   select(-dpt_cdc.y, dpt_cdc = dpt_cdc.x, -fecha_fallecimiento)
@@ -56,14 +65,23 @@ province_baseline <- purrr:::map(
     group_by(dpt_cdc, prov_cdc, pob) %>%
     summarize(x1 = min(x1)) %>%
     mutate(
-      x1 = x1 + .x,
-      # x1 = 0 + .x,
+      # x1 = x1 + .x,
+      x1 = 0 + .x,
       y1 = 0,
       ylog = log(0 + min(data_prov$y1)/2),
       n = 0,
-      sd = log(sqrt(1 / (y1 + min(data_prov$y1)/2)))
-      # sd = 1
-      # sd = log(sqrt(1 / (y1 + 0.0000001)))
+      # sd = 
+      #   sqrt(
+      #     (y1 + min(data_prov$y1)/2) * (1 - (y1 + min(data_prov$y1)/2)) / pob
+      #     )
+      
+      sd = 1,
+      # sd = sqrt(
+      #   (1 - (y1 + min(data_prov$y1)/2)) /
+      #     ((y1 + min(data_prov$y1)/2) * pob)
+      #   ),
+      y_low = ylog - (1.96 * sd),
+      y_upp = ylog + (1.96 * sd)
     )
   ) %>%
   bind_rows()
@@ -97,16 +115,16 @@ mod_prov <- MRBRT(
       alt_cov = "x1",
       use_spline = TRUE,
       # spline_knots = array(c(0, 0.25, 0.5, 0.75, 1)),
-      spline_knots = array(c(seq(0, 1, by = 0.2))),
+      spline_knots = array(c(seq(0, 1, by = 0.1))),
       spline_degree = 2L,
       spline_knots_type = 'frequency',
       # spline_r_linear = FALSE,
       # spline_l_linear = TRUE,
-      # # prior_spline_convexity_domain = array(c(0, 0.03)),
-      prior_spline_monotonicity = 'increasing',
-      prior_spline_monotonicity_domain = array(c(0, 0.05))
-      # prior_spline_convexity = "convex"
-      # prior_spline_maxder_gaussian = array(c(0, 0.01))
+      # prior_spline_convexity = "convex",
+      # prior_spline_convexity_domain = array(c(0, 0.03))
+      # prior_spline_monotonicity = 'increasing',
+      # prior_spline_monotonicity_domain = array(c(0, 0.01))
+      prior_spline_maxder_gaussian = array(c(0, 0.05))
     )
   )
 )
@@ -123,11 +141,11 @@ dat_pred_prov$load_df(
 
 pred_prov <- mod_prov$predict(data = dat_pred_prov)
 
-ggplot(data_prov) +
-  geom_point(aes(x = x1, y = y1 * 100000), alpha = 0.3, size = 0.3) +
-  geom_line(data = df_pred, aes(x = x1, y = exp(pred_prov) * 100000)) +
-  labs(x = "Week", y = "Mortality per 100,000") +
-  theme_bw()
+# ggplot(data_prov) +
+#   geom_point(aes(x = x1, y = y1 * 100000), alpha = 0.3, size = 0.3) +
+#   geom_line(data = df_pred, aes(x = x1, y = exp(pred_prov) * 100000)) +
+#   labs(x = "Week", y = "Mortality per 100,000") +
+#   theme_bw()
 
 # Cascade splines Dep  ----------------------------------------------------
 
@@ -157,12 +175,13 @@ pred_cascade_dpto <- predict_spline_cascade(
   ) %>% 
   left_join(data_prov)
 
-ggplot(data = pred_cascade_dpto) +
-  geom_line(aes(x = x1, y = exp(pred) * 100000)) +
-  geom_point(aes(x = x1, y = y1 * 100000), size = 0.1, alpha = 0.5) +
-  facet_wrap(.~dpt_cdc) +
-  labs(x = "Week", y = "Mortality per 100,000") +
-  theme_bw()
+# ggplot(data = pred_cascade_dpto) +
+#   geom_line(aes(x = x1, y = exp(pred) * 100000)) +
+#   geom_point(aes(x = x1, y = y1 * 100000), size = 0.1, alpha = 0.5) +
+#   facet_wrap(.~dpt_cdc) +
+#   ylim(c(0,50)) +
+#   labs(x = "Week", y = "Mortality per 100,000") +
+#   theme_bw()
 
 # Cascade splines Prov --------------------------------------------------------
 
@@ -173,7 +192,7 @@ mod_spline_prov <- run_spline_cascade(
   col_obs_se = "sd",
   col_study_id = "id",
   stage_id_vars = c("dpt_cdc", "prov_cdc"),
-  thetas = c(1,5),
+  thetas = c(10, 10),
   output_dir = "output/",
   model_label = "mbrt_cascade_peru_prov",
   overwrite_previous = TRUE
@@ -195,37 +214,108 @@ preds_cascade_prov <- predict_spline_cascade(
   left_join(data_prov) 
 
 depts <- unique(data_prov$dpt_cdc)
+# depts <- "LORETO"
 
 for (i in depts) {
   
-  graph_dpto <- pred_cascade_dpto %>% 
-    filter(dpt_cdc == i) %>% 
+  graph_dpto <- pred_cascade_dpto %>%
+    filter(dpt_cdc == i) %>%
     ggplot() +
-    geom_line(aes(x = x1, y = exp(pred) * 100000)) +
+    geom_errorbar(
+      aes(
+        x = x1,
+        y = y1 * 1e5,
+        ymin = exp(y_low) * 1e5,
+        ymax = exp(y_upp) * 1e5
+      ),
+      size = 0.2) +
     geom_point(
-      data = data_prov  %>% filter(dpt_cdc == i), 
-      aes(x = x1, y = y1 * 100000), 
-      size = 0.7, 
+      data = data_prov  %>% filter(dpt_cdc == i),
+      aes(x = x1, y = y1 * 100000),
+      size = 0.7,
       alpha = 0.4
     ) +
+    geom_line(aes(x = x1, y = exp(pred) * 100000), col = "red") +
     facet_wrap(.~dpt_cdc) +
     labs(x = NULL, y = "Mortality per 100,000") +
     theme_bw()
-  
-  graph_prov <- preds_cascade_prov %>% 
-    filter(dpt_cdc == i) %>% 
+
+  graph_prov <- preds_cascade_prov %>%
+    filter(dpt_cdc == i) %>%
     ggplot() +
-    geom_line(aes(x = x1, y = exp(pred) * 100000)) +
     geom_point(aes(x = x1, y = y1 * 100000), size = 1, alpha = 0.5) +
+    geom_errorbar(
+      aes(
+        x = x1,
+        y = y1 * 1e5,
+        ymin = exp(y_low) * 1e5,
+        ymax = exp(y_upp) * 1e5
+        ),
+      size = 0.2) +
+    geom_line(aes(x = x1, y = exp(pred) * 100000), col = "blue") +
     facet_wrap(.~prov_cdc) +
+    coord_cartesian(
+      ylim = c(
+        0,
+        max(
+          preds_cascade_prov %>% filter(dpt_cdc == i) %>% .$y1,
+          na.rm = TRUE
+          ) * 1e5 + 5
+        )
+      )+
     labs(x = "Week", y = "Mortality per 100,000") +
-    theme_bw() 
-  
+    theme_bw()
+
   graph <-  ggdraw() +
     draw_plot(graph_dpto, x = 0.25, y = .7, width = .5, height = .3) +
-    draw_plot(graph_prov, x = 0, y = 0, width = 1, height = 0.7) 
+    draw_plot(graph_prov, x = 0, y = 0, width = 1, height = 0.7)
   
+  # graph_dpto_log <- pred_cascade_dpto %>%
+  #   filter(dpt_cdc == i) %>%
+  #   ggplot() +
+  #   geom_errorbar(
+  #     aes(
+  #       x = x1,
+  #       y = ylog,
+  #       ymin = y_low,
+  #       ymax = y_upp
+  #     ),
+  #     size = 0.2) +
+  #   geom_point(
+  #     data = data_prov  %>% filter(dpt_cdc == i),
+  #     aes(x = x1, y = ylog),
+  #     size = 0.7,
+  #     alpha = 0.4
+  #   ) +
+  #   geom_line(aes(x = x1, y = pred), col = "red") +
+  #   facet_wrap(.~dpt_cdc) +
+  #   labs(x = NULL, y = "Mortality per 100,000") +
+  #   theme_bw()
+  # 
+  # graph_prov_log <- preds_cascade_prov %>%
+  #   filter(dpt_cdc == i) %>%
+  #   ggplot() +
+  #   geom_point(aes(x = x1, y = ylog), size = 1, alpha = 0.5) +
+  #   geom_errorbar(
+  #     aes(
+  #       x = x1,
+  #       y = ylog,
+  #       ymin = y_low,
+  #       ymax = y_upp
+  #     ),
+  #     size = 0.2) +
+  #   geom_line(aes(x = x1, y = pred), col = "blue") +
+  #   facet_wrap(.~prov_cdc) +
+  #   labs(x = "Week", y = "Mortality per 100,000") +
+  #   theme_bw()
+  # 
+  # graph_log <-  ggdraw() +
+  #   draw_plot(graph_dpto_log, x = 0.25, y = .7, width = .5, height = .3) +
+  #   draw_plot(graph_prov_log, x = 0, y = 0, width = 1, height = 0.7)
+  # 
+  # print(graph_log)
   print(graph)
   
   # ggsave(plot = graph, filename =  paste0("plots/sd_one/",i, ".png"), scale = 2)
 }
+ 
