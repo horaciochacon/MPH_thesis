16a8198658f2288adf967853031405e50193bb40
Horacio Chacón <horaciochacon89@gmail.com>
2022-05-10 00:21:23 -0700

    Rt estimation

--- src/03_Epidemic_estimation.R
+++ src/03_Epidemic_estimation.R
@@ -5,97 +5,73 @@ library(ggrepel)
 library(EpiEstim)
 library(incidence)
 library(lubridate)
+library(tidyr)
 
+
+# Reading the necessary data -----------------------------------------------
 
 # Province level population
 poblacion_prov <- read.csv("data/poblacion_provincial_peru.csv") %>% 
   group_by(prov_cdc = PROVINCIA) %>% 
   summarise(pob = sum(POBLACION)) 
 
-# Read predicted mortality time series ------------------------------------
+# Read Time-varying 
+peru_ifr <- read.csv("data/peru_ifr.csv") %>% 
+  as_tibble() %>%  mutate(date = as_date(date)) %>% 
+  filter(between(date, as_date("2020-03-02"), as_date("2021-09-13"))) %>% 
+  mutate(mean = ifelse(is.infinite(mean), NA, ifelse(mean==0,0.0161,mean))) %>% 
+  fill(mean, .direction = "up") %>% 
+  select(date, ifr = mean)
+
+# Read predicted mortality time series 
 prov_preds <- read.csv("data/pred_prov_time_series.csv") %>% 
+  mutate(day = round(x1 * 7, digits = 1), .after = x1) %>% 
+  filter(day %in% 0:560) %>% 
+  distinct(day, prov_cdc, .keep_all = TRUE) %>% 
   tibble() %>% 
+  select(day, prov_cdc, dpt_cdc, pred, mortality) %>% 
+  mutate(date = as_date(day + 18323), .before = day) %>% 
+  left_join(peru_ifr) %>% 
   left_join(poblacion_prov) %>% 
   mutate(
-    days = lubridate::as_date(x1 * 7,),
     D = mortality * pob / 7,
-    I = D / 0.007
-    ) %>% 
-  select(days, I, prov_cdc) %>% 
+    I = D / ifr
+  ) %>% 
+  select(date, I, prov_cdc) %>% 
   split(.$prov_cdc)
 
-d <- prov_preds[["LORETO"]] %>% 
-  select(I, days) 
-
-inc <- as.incidence(x = d$I, interval = 7, dates = d$days)
-
-plot(inc)
-
-
-
 # Estimating Rt -----------------------------------------------------------
 
-res_parametric_si <- estimate_R(
-  incid = d,
-  method = "parametric_si",
-  config = make_config(
-    list(
-      mean_si = 5.4,
-      std_si = 0.11
-    )
+rt_list <- prov_preds %>% 
+  map(
+    ~estimate_R(
+      .x, "parametric_si", 
+      config = make_config(list(mean_si = 5.4, std_si = 0.11)))
   )
+
+rt_peak <- rt_list %>% 
+  imap(
+    ~.x$R %>% 
+      filter(`Quantile.0.025(R)` > 1) %>% 
+      filter(`Quantile.0.975(R)` - `Quantile.0.025(R)` < 0.5 ) %>% 
+      filter(`Quantile.0.025(R)` == max(`Quantile.0.025(R)`)) %>% 
+      mutate(prov_cdc = .y, .before = t_start) %>% 
+      select(1,3,4,6)
+  ) 
+
+rt_peak_df <- rt_peak %>% 
+  bind_rows()
+
+# Plots -------------------------------------------------------------------
+
+map2(
+  rt_list, 
+  rt_peak,
+  .f = 
+    ~estimate_R_plots(.x, what = "R") + 
+    geom_hline(yintercept = 1) +
+    labs(title = .y$prov_cdc) +
+    geom_vline(xintercept = .y$t_end)
 )
 
-plot(res_parametric_si, legend = FALSE)                                
 
-g <- res_parametric_si$R %>% 
-  ggplot(aes(x = t_start, y = `Median(R)`)) +
-  geom_ribbon(
-    aes(ymin = `Quantile.0.025(R)`, ymax = `Quantile.0.975(R)` ),
-    fill = "lightgray"
-    ) +
-  geom_line() +
-  ylim(c(0,2.5)) +
-  geom_hline(yintercept = 1,lty = 2)
-
-h <- d %>% 
-  ggplot(aes(x = days, y = I)) +
-  geom_line()
-
-ggarrange(g,h,nrow = 2)
-
-View(res_parametric_si$R)
-
-
-# Estimating Rt -----------------------------------------------------------
-
-rts <- prov_preds[1:2] %>% 
-  purrr::map(
-     . %>% 
-      select(I, days) %>% 
-      estimate_R(
-         incid = .,
-         method = "parametric_si",
-         config = make_config(
-           list(
-             mean_si = 5.4,
-             std_si = 0.11
-           )
-         )
-       )
-  )
-
-
-for (i in 1:2) {
-  g <- rts[[i]]$R %>% 
-    ggplot(aes(x = t_start, y = `Median(R)`)) +
-    geom_ribbon(
-      aes(ymin = `Quantile.0.025(R)`, ymax = `Quantile.0.975(R)` ),
-      fill = "lightgray"
-    ) +
-    geom_line() +
-    geom_hline(yintercept = 1,lty = 2) +
-    ylim(c(0,5))
-  
-  print(g)
-}
